9820a099c7c28ee2fd567762db57a9ffc30a8f87
scottzyang <scottzyang@gmail.com>
2022-11-15 13:37:46 -0600

    Create temporary working route with predetermined id Fix jinja syntax errors in character.html

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 # imports
 from flask import Flask, request, render_template
+import json
+import requests
 
 # initialize app
 app = Flask(__name__)
 
 # set API URL
-TENOR_URL = 'https://swapi.py4e.com/api/'
+SWAPI_URL = 'https://swapi.py4e.com/api/people/1'
 
 # create route for SWAPI results
-@app.route('/character_results', methods=['GET'])
+@app.route('/character', methods=['GET'])
 def character_search():
-  pass
-  return render_template('character.html')
+  # character_id = request.args.get('character-id')
+
+  response = requests.get(
+    SWAPI_URL)
+
+  character_info = response.json()
+
+  context = {
+    'character_info': character_info
+  }
+
+  return render_template('character.html', **context)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

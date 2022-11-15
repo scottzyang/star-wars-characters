1ef5253e6553dfa54420e3dc69f73c9d6f9907aa
scottzyang <scottzyang@gmail.com>
2022-11-15 12:41:32 -0600

    Create baseline route

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from flask import Flask, request
+# imports
+from flask import Flask, request, render_template
+
+# initialize app
+app = Flask(__name__)
 
 # set API URL
 TENOR_URL = 'https://swapi.py4e.com/api/'
+
+# create route for SWAPI results
+@app.route('/character_results', methods=['GET'])
+def character_search():
+  pass
+  return render_template('character.html')
